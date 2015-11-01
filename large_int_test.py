e309b532f8e8ab05626adde06d775478312e2e02
Jacob Reed <jake32321@yahoo.com>
2015-10-31 22:12:06 -0500

    new file using large intgers

---
+++ large_int_test.py
@@ -0,0 +1,8 @@
+__author__ = 'jacobreed'
+#Simply adds and tests the functionality of large integer outputs. For large test cases.
+a = int(raw_input())
+b = int(raw_input())
+c = int(raw_input())
+d = int(raw_input())
+
+print pow(a,b)+pow(c,d)
