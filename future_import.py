49f7f3c0acb9df669944eb9b51d81c1dfccbdb8b
Jacob Reed <jake32321@yahoo.com>
2015-10-31 21:41:43 -0500

    added new files and changed extensions

---
+++ future_import.py
@@ -0,0 +1,8 @@
+#Uses __future__ to allow for both float and integer division.
+from __future__ import division
+
+a = int(raw_input())
+b = int(raw_input())
+
+print a//b
+print a/b
---
+++ raw_input_test.py
@@ -0,0 +1,5 @@
+#Tests the raw input fucntion of python
+a = int(raw_input())
+b = int(raw_input())
+
+print a+b+"\n"+a-b+"\n"+a*b+"\n"
